8d4ef0508268968d07200fc35c7e32b08fbf54b0
Ben Cady <bjcady14@gmail.com>
2021-04-12 16:43:22 -0500

    added scripts folder

---
+++ scripts/apply.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ../helm-chart-copy
+touch tempLogs.txt
+date >> bashLogs.txt 
+helm upgrade kube-prometheus-stack prometheus-community/kube-prometheus-stack -f values.yaml >> tempLogs.txt
+cat tempLogs.txt
+cat tempLogs.txt >> bashLogs.txt
+rm tempLogs.txt
+echo ' ' >> bashLogs.txt
+
+#This script allows you to update the helm chart if you make any changes to the values.yaml
