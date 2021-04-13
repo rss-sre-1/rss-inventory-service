211ebe5a41faab8fad239bc66dac4cb2b4598175
Ben Cady <bjcady14@gmail.com>
2021-04-12 21:30:56 -0500

    removed unnecessary steps from apply.sh

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 cd ../helm-chart-copy
-touch tempLogs.txt
-date >> bashLogs.txt 
+touch tempLogs.txt 
 helm upgrade kube-prometheus-stack prometheus-community/kube-prometheus-stack -f values.yaml >> tempLogs.txt
 cat tempLogs.txt
-cat tempLogs.txt >> bashLogs.txt
 rm tempLogs.txt
-echo ' ' >> bashLogs.txt
 
 # This script allows you to update the helm chart if you make any changes to the values.yaml
